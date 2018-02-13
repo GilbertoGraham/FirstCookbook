6139d7cd2c3d588a450908c16de51b0fef35a4b9
root <root@mnhqfelt15vm01.ltfinc.net>
2018-02-13 18:36:45 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+package 'tree' do
+	action :install
+end
+
+package 'ntp'
+
+package 'git' do
+	action :install
+end
+
+
+file '/etc/motd' do
+	content 'Property of Gilberto Graham'
+	action :create
+	owner 'root'
+	group 'root'
+end
+
+service 'ntpd' do
+	action [ :enable, :start ]
+end
