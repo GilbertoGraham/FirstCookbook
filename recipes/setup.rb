64b14ec018a6396c43200cd8840f0c82cbf39127
Gilberto Graham <betograhaam@gmail.com>
2018-02-15 20:55:31 +0000

    Last changes

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,9 +10,20 @@ package 'git' do
 	action :install
 end
 
+#node['ipaddress']
+#node['memory']['total'] 
+
+# print statements "i have 4 apples"
+#apple_count = 4
+#puts "I have #{apple_count} apples"
 
 file '/etc/motd' do
-	content 'Property of Gilberto Graham'
+	content "Property of Gilberto Graham
+	HOSTNAME: #{node['hostname']}
+	IPADDRESS: #{node['ipaddress']} 
+	CPU: #{node['cpu']['0']['mhz']}
+	MEMORY:  #{node['memory']['total']}
+"
 	action :create
 	owner 'root'
 	group 'root'
