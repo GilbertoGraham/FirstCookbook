ef002d180607aa305c0e858769e8a24c6f3a83a5
Gilberto Graham <betograhaam@gmail.com>
2018-02-15 22:43:37 +0000

    refactored motd file with a template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,23 +10,9 @@ package 'git' do
 	action :install
 end
 
-#node['ipaddress']
-#node['memory']['total'] 
-
-# print statements "i have 4 apples"
-#apple_count = 4
-#puts "I have #{apple_count} apples"
-
-file '/etc/motd' do
-	content "Property of Gilberto Graham
-	HOSTNAME: #{node['hostname']}
-	IPADDRESS: #{node['ipaddress']} 
-	CPU: #{node['cpu']['0']['mhz']}
-	MEMORY:  #{node['memory']['total']}
-"
+template '/etc/motd' do
+	source 'motd.erb'
 	action :create
-	owner 'root'
-	group 'root'
 end
 
 service 'ntpd' do
